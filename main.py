d3ef6dd7981d2413b8ad07ca754223a76163662c
ZhuElly <34325707+Elly2018@users.noreply.github.com>
2026-08-01 00:03:14 +0800

    Added flask in it

--- main.py
+++ main.py
@@ -2,39 +2,38 @@ import asyncio
 import os
 import sys
 from google.antigravity import Agent, LocalAgentConfig
+from flask import Flask, jsonify, request
 
-GEMINI_API_KEY = sys.argv[1]
+app = Flask(__name__)
+
+GEMINI_API_KEY = sys.argv[1].split(',')
 
 print(GEMINI_API_KEY)
 
-async def main() -> None:
-    # Let's get a little meta: We are loading the real 'google-antigravity-sdk' skill
-    # that teaches this agent how to build with the very SDK it is running on! 🧠
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    skill_path = os.path.abspath(
-        os.path.join(script_dir, "../skills")
-    )
+@app.route("/api/chat", methods=["GET"])
+async def get_all_books():
+  script_dir = os.path.dirname(os.path.abspath(__file__))
+  skill_path = os.path.abspath(
+      os.path.join(script_dir, "../skills")
+  )
+  
+  config = LocalAgentConfig(
+    model="gemini-3.1-flash-lite",
+    api_key=GEMINI_API_KEY, 
+    skills_paths=[skill_path]
+  )
+  
+  async with Agent(config) as my_agent:
+    # Ask the agent what skills it has.
+    prompt = "What available skills do you have?"
+    print(f"  User: {prompt}")
 
-    print(f"  Loading skills from: {skill_path}")
+    response = await my_agent.chat(prompt)
 
-    # Configure the agent with the skills path.
-    config = LocalAgentConfig(
-      model="gemini-3.1-flash-lite",
-      api_key=GEMINI_API_KEY, 
-      skills_paths=[skill_path]
-    )
-
-    async with Agent(config) as my_agent:
-        # Ask the agent what skills it has.
-        prompt = "What available skills do you have?"
-        print(f"  User: {prompt}")
-
-        response = await my_agent.chat(prompt)
-
-        # Await the full aggregated text response.
-        response_text = await response.text()
-        print(f"  Agent: {response_text}")
-
+    # Await the full aggregated text response.
+    response_text = await response.text()
+    print(f"  Agent: {response_text}")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(app.run(debug=True, port=8080))
+    
--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ REQ_FILE="requirements.txt"
 python3 -m venv "$ENV_DIR"
 source "$ENV_DIR/bin/activate"
 pip install --upgrade pip setuptools wheel
-pip install --no-cache-dir -r "$REQ_FILE" --default-timeout=1000
+pip install -r "$REQ_FILE" --default-timeout=1000
